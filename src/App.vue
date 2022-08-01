d164d8aec559af708de3d81bc2a26013531a8982
Milena Ogloblina <ogloblina.milena@gmail.com>
2022-08-01 12:22:35 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-main>
+      <!--      <ChatPage></ChatPage>-->
+      <ChatPage/>
+    </v-main>
+
+  </v-app>
+
+</template>
+<script>
+import ChatPage from './components/ChatPage';
+export default {
+  name: 'App',
+  components: {
+    ChatPage,
+  },
+  data: () => ({}),
+}
+
+</script>
